0a85dc8e9b75159b4f49b9a386fca5c0a891b82c
Christy Lum <christy.lum121@gmail.com>
2020-07-30 16:54:22 +0000

    Cleaned up code for survey.js

--- survey.js
+++ survey.js
@@ -11,11 +11,6 @@ const petProfile = () => {
       rl.question('Do they have a name or nickname?', (name) => {
         rl.question('What do you like to do with your furry friend?', (activity) => {
           rl.question('What\'s their favourite treat?', (food) => {
-            // console.log(`Yeah ${food}'s  gross. Me neither`);
-            // console.log(`No way. Didnt think you were a ${music} fan!`);
-            // console.log(`Thats Cool, I like ${activity} too.`);    
-            // console.log(`Thank you for sharing your nickname: ${nickname}`);
-            // console.log(`Thank you for your valuable feedback: ${answer}`);
             console.log(`Your pet is the cutest thing in the world! It is a ${breed}! When you need to get their attention, you say their name: ${name}! In my spare time, I bring ${name} to go and ${activity}.\nMy pet's favourite treat is ${food}.`);
           rl.close();
           });
